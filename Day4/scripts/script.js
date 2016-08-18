a34a836eace2ab9fdf36d8a60dec165fb4bc4d76
Nataly Nerovnya <natanerovnja@gmail.com>
2016-08-18 11:50:27 +0300

    nothing

---
+++ Day4/scripts/script.js
@@ -0,0 +1,10 @@
+$(function() {
+	$("#btnGenerate").on("click", generate);
+
+	function random(min, max) {
+        return Math.floor((Math.random() * max) + min);
+    }
+
+	function generate() {
+	}
+});
